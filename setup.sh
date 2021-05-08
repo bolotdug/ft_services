1b9fefc4efd9999b14235be76b1c80cc4ffb3d87
bolotdug <bolotdug@gmail.com>
2021-05-08 03:28:21 +0300

    in folder

---
+++ setup.sh
@@ -0,0 +1,14 @@
+minikube stop
+minikube delete
+minikube start --vm-driver=virtualbox --cpus=4 --memory=4G
+minikube addons enable dashboard
+minikube addons enable metallb
+eval $(minikube -p minikube docker-env)
+# kubectl delete deploy nginx-deployment
+
+kubectl apply -f configmap.yaml
+
+docker build -t nginx-image ./nginx
+kubectl apply -f ./nginx/nginx.yaml
+
+minikube dashboard &
